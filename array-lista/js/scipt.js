13f30ac2c571dddce43e77d2cecb75eb09b85b57
tomasdarienko <darienkotomas@gmail.com>
2023-11-24 17:58:54 +0100

    fixed

--- array-lista/js/scipt.js
+++ array-lista/js/scipt.js
@@ -1,15 +1,22 @@
+// creo l array con i vari elementi
 let array = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
+
+
 // let cerca = prompt('cerca nel frigo')
 
 
-
+// assegno un valore di default a flag
 let flag = false
+
+// creo un ciclo for che si ripete il numero di volte quanti sono gli elementi nel array
 for(i=0 ; i<array.length;i++){
+     // se nel array ce "cocomero" flag diventa true
      if(array[i] == "cocomero"){
           flag=true
      }
 }
 
+// se flag e diventato true abbiamo il cocomero altrimenti no
 if(flag){
      console.log("Trovato! Devo solo preparare il cocktail.")
 }
--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,9 +1,27 @@
+// dichiaro le variabili
+let ncasuale, N, array, lung
 
-let N = prompt('inserisci un numero')
+
+// faccio inserire un numero
+N = prompt('inserisci un numero (valore min "5")')
 
 
+// creo un array vuoto
+array = []
 
-for(let i=0;i<N;i++){
-let Ncasuale = Math.round(Math.random() * (100 - 1) + 1);
-console.log(Ncasuale);
+
+// creo un ciclo for contenente una variabile che genera un numero da 1 a 100 che si ripete il numero di volte inserito dall utente
+for (let i = 0; i < N; i++) {
+     ncasuale = Math.round(Math.random() * (100 - 1) + 1);
+
+     // infilo ogni numero casuale nel array
+     array.push(ncasuale)
 }
+
+
+
+// seleziono gli ultimi 5 elementi del array
+
+// console.log(array);
+lung = array.slice(-5);
+console.log(lung)
